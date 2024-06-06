380212438bc07218032f6ac5b0b55b5d8577dbf1
MattiaSarti <nmattia1@gmail.com>
2024-06-06 16:56:25 +0200

    fix: 🌸 send button never hidden yet disabled when necessary 🌸

--- assets/send_button.js
+++ assets/send_button.js
@@ -5,19 +5,6 @@ import ThemeContext from '../src/components/Widget/ThemeContext';
 function Send({ ready }) {
   const { mainColor } = useContext(ThemeContext);
 
- if (!ready) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="40"
-      height="40"
-      viewBox="0 0 40 40"
-      fill="none"
-    >
-    </svg>
-  );
- }
-
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
